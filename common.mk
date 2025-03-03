56ed2f6ba990173b2d646eb66bc1ffc68f8a39bf
klozz <carlosj@klozz.dev>
2025-03-03 21:31:52 +0000

    sm8650-common: adapt to xperience
    Signed-off-by: klozz <carlosj@klozz.dev>

--- common.mk
+++ common.mk
@@ -503,7 +503,7 @@ PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
     hardware/google/interfaces \
     hardware/google/pixel \
-    hardware/lineage/interfaces/power-libperfmgr \
+    hardware/xperience/interfaces/power-libperfmgr \
     hardware/oplus \
     hardware/qcom-caf/common/libqti-perfd-client \
     kernel/oneplus/sm8650 \
@@ -564,8 +564,8 @@ PRODUCT_COPY_FILES += \
 $(call soong_config_set,qti_thermal,netlink,true)
 
 # Touch
+#    TouchGestures
 PRODUCT_PACKAGES += \
-    TouchGestures \
     vendor.lineage.touch@1.0-service.oplus
 
 $(call soong_config_set,OPLUS_LINEAGE_TOUCH_HAL,INCLUDE_DIR,$(LOCAL_PATH)/touch/include)
