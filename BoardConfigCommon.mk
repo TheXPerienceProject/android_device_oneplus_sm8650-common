c6a667298a2390a9b57ccccda37c5cdfb4c010e7
klozz <carlosj@klozz.dev>
2025-03-06 03:32:49 +0000

    sm8650-common: Build aperture
    Signed-off-by: klozz <carlosj@klozz.dev>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -115,6 +115,7 @@ BOARD_BOOTCONFIG := \
     androidboot.vendor.qspa=true \
     androidboot.usbcontroller=a600000.dwc3 \
     androidboot.console=0
+#    androidboot.selinux=permissive
 
 BOARD_USES_GENERIC_KERNEL_IMAGE := true
 BOARD_KERNEL_BASE := 0x00000000
