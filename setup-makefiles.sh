c26b77ec888fe74fa7aebc72260b04e58032b8d0
klozz <carlosj@klozz.dev>
2025-07-30 04:59:35 +0000

    Revert "sm8650-common: Switch to oss audio HAL"
    This reverts commit b09728a769982567a7b70699af66949a3aa402c5.

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -74,12 +74,6 @@ function lib_to_package_fixup_vendor_variants() {
             ;;
         libagmclient | \
             libpalclient | \
-            libar-pal | \
-            libar-acdb | \
-            libar-gsl | \
-            liblx-osal | \
-            libats | \
-            libagm | \
             libwpa_client) ;;
         *)
             return 1
