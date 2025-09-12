dcac8c18808807b90d4384965766e9bfcda2f6c8
anky894 <ko282ko82@gmail.com>
2025-09-12 17:31:53 +0530

    sm8650-common: Decommonize parts to support avalon, lexus and caihong
    This change decommonizes the following things:
    * Camera - not compatible with other devices
    * Color modes overlay - avalon uses different color modes
    * Display/QSH sensors HAL - breaks caihong
    * EVA firmware - not compatible, camera remains black on avalon
    * Graphics firmware - breaks avalon
    * NFC blobs - not compatible with other devices
    * Vibrator service and overlay - avalon uses RichTap service
    * VPU firmware - breaks avalon camera
    * Wi-Fi firmware symlinks - avalon uses qca6750 WLAN variant
    * WCNSS_qcom_cfg.ini - not compatible with other devices
    * ADSP modules waffle's causes dead speaker on lexus
    * ANT+ firmware are target specific
    * Neural network binaries are target specific
    * tfa is device specific audio lib
    * ufs firmware are target specific
    * vibrator libs depends on what vibrator hal target uses
    * remove target specific code such as ltpo, EUICC, charger fw and alert slider
    
    Co-authored-by: inferno0230 <mail@inferno0230.in>
    co-authored-by: grepfox <grepfox@tutamail.com>
    co-authored-by: chandu078 <chandudyavanapelli03@gmail.com>
    Change-Id: Id7c368ed8789cae86f0536927eb53f08e5a8c7a5
    Signed-off-by: klozz <carlosj@klozz.dev>

--- common.mk
+++ common.mk
@@ -31,11 +31,6 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_VIRTUAL_AB_COMPRESSION_METHOD := lz4
 
-# Alert slider
-PRODUCT_PACKAGES += \
-    KeyHandler \
-    tri-state-key-calibrate
-
 #aperture
 PRODUCT_PACKAGES += \
     Aperture
@@ -578,7 +573,6 @@ PRODUCT_COPY_FILES += \
 
 # Telephony
 PRODUCT_PACKAGES += \
-    OplusEuicc \
     extphonelib \
     extphonelib-product \
     extphonelib.xml \
@@ -597,7 +591,6 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.cdma.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.cdma.xml \
-    frameworks/native/data/etc/android.hardware.telephony.euicc.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.telephony.euicc.xml \
     frameworks/native/data/etc/android.hardware.telephony.gsm.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.gsm.xml \
     frameworks/native/data/etc/android.hardware.telephony.ims.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.ims.xml \
     frameworks/native/data/etc/android.hardware.telephony.mbms.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.mbms.xml \
@@ -663,14 +656,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.verified_boot.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.verified_boot.xml
 
 # Vibrator
-PRODUCT_PACKAGES += \
-    vendor.qti.hardware.vibrator.service
-
-$(call soong_config_set,qti_vibrator,use_effect_stream,true)
-$(call soong_config_set,qti_vibrator,use_effect_stream_strength,true)
-$(call soong_config_set,qti_vibrator,use_primitive_effect_stream,true)
-$(call soong_config_set,qti_vibrator,effect_lib,libqtivibratoreffect.oplus_sm8650-richtap)
-
 PRODUCT_COPY_FILES += \
     vendor/qcom/opensource/vibrator/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
 
@@ -701,9 +686,7 @@ PRODUCT_COPY_FILES += \
 
 # WiFi firmware symlinks
 PRODUCT_PACKAGES += \
-    firmware_wlanmdsp.otaupdate_symlink \
-    firmware_wlan_mac.bin_symlink \
-    firmware_WCNSS_qcom_cfg.ini_symlink
+    firmware_wlanmdsp.otaupdate_symlink
 
 # Inherit from the proprietary files makefile.
 $(call inherit-product, vendor/oneplus/sm8650-common/sm8650-common-vendor.mk)
