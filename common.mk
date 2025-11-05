e3d01ba9aafeefa4366dbf83746c82135c369d63
klozz <carlosj@klozz.dev>
2025-11-05 01:51:42 -0600

    sm8650-common: move audio_effects from qcom/hardware to device tree
    we want now vyper4android
    
    Change-Id: Ic44e7d048209ff2e36d1ae66b055d12c34d1615f
    Signed-off-by: klozz <carlosj@klozz.dev>

--- common.mk
+++ common.mk
@@ -97,7 +97,7 @@ $(call soong_config_set,android_hardware_audio,skip_speaker_layout_channel_mask_
 PRODUCT_COPY_FILES += \
 $(foreach DEVICE_SKU, $(QCV_FAMILY_SKUS), \
     $(CONFIG_HAL_SRC_DIR)/audio_effects.conf:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_$(DEVICE_SKU)/audio_effects.conf \
-    $(CONFIG_HAL_SRC_DIR)/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_$(DEVICE_SKU)/audio_effects.xml \
+    $(LOCAL_PATH)/audio/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_$(DEVICE_SKU)/audio_effects.xml \
     $(LOCAL_PATH)/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio/sku_$(DEVICE_SKU)/audio_policy_configuration.xml)
 
 PRODUCT_COPY_FILES += \
