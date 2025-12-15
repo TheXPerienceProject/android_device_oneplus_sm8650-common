50e3bf0c2fa68ab848c1b71065d5dec2155d32ce
John Galt <johngaltfirstrun@gmail.com>
2025-12-15 13:25:08 -0500

    sm8650-common: modify libtensorflowlite_c vendor soname
    Ours is needed for a few blobs
    
    Change-Id: Ic0907ff80bee0c2708a650d53b408d6f2f3ae6fe
    Signed-off-by: klozz <carlosj@klozz.dev>

--- extract-files.sh
+++ extract-files.sh
@@ -124,15 +124,6 @@ function blob_fixup() {
             [ "$2" = "" ] && return 0
             "${PATCHELF}" --replace-needed "libprotobuf-cpp-full.so" "libprotobuf-cpp-full-21.7.so" "${2}"
             ;;
-        vendor/lib64/libdpps.so|vendor/lib64/libsnapdragoncolor-manager.so \
-       |odm/lib64/libdisplaycolorfeature.so \
-       |odm/lib64/libdisplayfossfeature_nature.so \
-       |vendor/bin/qvrdatauploader \
-       |odm/bin/hw/vendor-oplus-hardware-touch-V2-service \
-       |odm/bin/touchDaemon)
-            [ "$2" = "" ] && return 0
-            "${PATCHELF}" --replace-needed "libtinyxml2.so" "libtinyxml2_stock.so" "${2}"
-            ;;
         vendor/lib64/libcapiv2uvvendor.so|vendor/lib64/liblistensoundmodel2vendor.so \
 	|vendor/lib64/libVoiceSdk.so)
             [ "$2" = "" ] && return 0
