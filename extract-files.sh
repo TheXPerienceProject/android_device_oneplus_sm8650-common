892bb0ad41d8fce69dde05d3e3e914930773000e
Hecheng Yu <kde-yyds@qq.com>
2026-03-27 17:00:51 +0800

    sm8650--common: add support for oneplus camera
    Change-Id: I2d743e34e9ae0f7410f946d52088e9220dd39f89
    Signed-off-by: klozz <carlosj@klozz.dev>

--- extract-files.sh
+++ extract-files.sh
@@ -105,6 +105,15 @@ function blob_fixup() {
             [ "$2" = "" ] && return 0
             "${PATCHELF}" --replace-needed "libstdc++.so" "libstdc++_vendor.so" "${2}"
             ;;
+        odm/lib64/libsharebuffer_impl.so)
+            [ "$2" = "" ] && return 0
+            "${PATCHELF}" --replace-needed "libutils.so" "libutils-stock.so" "${2}"
+            "${PATCHELF}" --replace-needed "libui.so" "libui-stock.so" "${2}"
+            ;;
+        vendor/lib64/libui-stock.so)
+            [ "$2" = "" ] && return 0
+            "${PATCHELF}" --replace-needed "android.hardware.graphics.common-V5-ndk.so" "android.hardware.graphics.common-V7-ndk.so"
+            ;;
         odm/lib64/vendor.oplus.hardware.virtual_device.camera.manager@1.0-impl.so|vendor/lib64/libcwb_qcom_aidl.so)
             [ "$2" = "" ] && return 0
             grep -q "libui_shim.so" "${2}" || "${PATCHELF}" --add-needed "libui_shim.so" "${2}"
